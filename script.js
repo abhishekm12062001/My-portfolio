f222b90463421614a3b3f91d6fd3bb7810a1f438
abhishekm12062001 <gowdaabhi522@gmail.com>
2024-03-25 12:55:09 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,29 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab")
+}
+
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200";
+}
+
+
+const msg = document.getElementById("msg")
+msg.innerHTML = "Message sent successfully"
+// setTimeout(function({
+//     msg.innerHTML="Message sent successfully"
+// }))
